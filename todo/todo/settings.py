a02f36efc0c72deb1be803ec878f56e65d5cec78
RadikSeyfullin <radikseyfullin@gmail.com>
2019-09-28 13:01:32 +0300

    Design select categories

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -76,6 +76,10 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'psycopg2': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'todo',
         'USER': 'radik',
